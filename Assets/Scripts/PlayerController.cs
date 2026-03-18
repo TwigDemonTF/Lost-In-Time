4bbae44c5834638f5a215fc20e8aa3b1e148fd6b
Corbex <ziggycorbey@gmail.com>
2026-03-18 12:56:35 +0100

    Input System
    Added a PlayerInputSystem for all inputs and basic movement (jump and horizontal movement) to it.
    Also added cinemacamera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInputSystem playerControls;
+    private Rigidbody2D rb;
+    private bool InAir = false;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHeight = 5f;
+    void Start()
+    {
+        playerControls = new PlayerInputSystem();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnJump()
+    {
+        if(InAir == false)
+        {
+        rb.AddForce(UnityEngine.Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        InAir = true;
+        }
+    }
+    private void OnMove(InputValue inputValue)
+    {
+        UnityEngine.Vector2 input = inputValue.Get<UnityEngine.Vector2>();
+
+        rb.linearVelocity = new UnityEngine.Vector2(input.x * moveSpeed, rb.linearVelocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            InAir = false;
+        }
+    }
+}
