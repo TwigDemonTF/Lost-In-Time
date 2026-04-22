146a69f68b10ea94e5b73a3e903e5a984d20f4da
Corbex <ziggycorbey@gmail.com>
2026-04-22 11:56:15 +0200

    boom boom boom boom BOOOM!!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     private float wallSlidingSpeed = 2f;
     // WallJumping
     private bool isWallJumping;
+    private bool CanWallJump = true;
     private float wallJumpingDirection = 0.4f;
     private float wallJumpingDuration = 0.4f;
     private Vector2 wallJumpingPower = new Vector2(4f, 9f); 
@@ -401,6 +402,10 @@ public class PlayerController : MonoBehaviour
 
     private IEnumerator WallJump()
     {
+        if (!CanWallJump)
+        {
+            yield break;
+        }
         if (isWallSliding)
         {
             isWallJumping = true;
@@ -422,7 +427,7 @@ public class PlayerController : MonoBehaviour
                 transform.localScale = localScale;
                 PlaySound(wallJumpSound, wallJumpVolume);
             }
-
+            CanWallJump = false;
             yield return new WaitForSeconds(wallJumpingDuration);
 
             StopWallJumping();
@@ -431,7 +436,8 @@ public class PlayerController : MonoBehaviour
     }
 
     private void StopWallJumping()
-    {
+    {   
+        CanWallJump = true;
         isWallJumping = false;
     }
 
